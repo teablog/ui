b19b585d8b55c16ebcac69698888613f0e879427
刘宁 <liuning@qutoutiao.net>
2020-08-06 23:34:40 +0800

    deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,4 @@ sudo docker pull registry.cn-hangzhou.aliyuncs.com/douyacun/tea-ui:latest
 pushd /data/web/ui/deployments/ui 
 sudo docker-compose up --force-recreate -d
 popd
-echo y|sudo docker images prune
+echo y|sudo docker image prune
