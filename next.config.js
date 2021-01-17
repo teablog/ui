d9f894883cf5bf922b0d8844368656fd1de11744
douyacun <douyacun@gmail.com>
2021-01-17 19:35:46 +0800

    cdn

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 const withCSS = require('@zeit/next-css')
 
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = withCSS({
     distDir: 'build',
+    assetPrefix: isProd ? process.env.CDN_HOST : '',
     webpack: function (config) {
       config.node = { fs: 'empty' };
       return config
