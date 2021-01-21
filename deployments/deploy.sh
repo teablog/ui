e968479579bc489cf085b4af00a83b0fd14d25f8
douyacun <douyacun@gmail.com>
2021-01-22 00:44:12 +0800

    docker-compose

--- deployments/deploy.sh
+++ deployments/deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# sudo docker pull registry.cn-hangzhou.aliyuncs.com/douyacun/tea-ui:latest
-# pushd /data/web/ui/deployments/ui 
-# sudo docker-compose up --force-recreate -d
-# popd
-# echo y|sudo docker image prune
+sudo docker pull registry.cn-hangzhou.aliyuncs.com/douyacun/tea-ui:latest
+pushd /data/web/ui
+sudo cnpm install
+popd
+pushd /data/web/ui/deployments/ui
+sudo docker-compose up --force-recreate -d
+popd
+echo y|sudo docker image prune
